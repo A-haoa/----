3b6ee6158fce1074c03a45837b3626e275152d3a
A-haoa <591065953@qq.com>
2022-03-01 15:22:47 +0800

    zhang

--- 测试1.js
+++ 测试1.js
@@ -3,4 +3,5 @@ document.getElementById("demo").innerHTML = "hello world!";
 function abc(a,b){
     return a*b;
 }
-document.write(Date());//直接写输出流
+
+
---
+++ 测试2.js
@@ -0,0 +1,6 @@
+
+document.getElementById("p2").innerHTML = add(3,4);
+function add(x,y){
+    return x+y;
+
+}
